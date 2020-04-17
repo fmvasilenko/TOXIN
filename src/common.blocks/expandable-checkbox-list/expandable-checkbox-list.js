3b7af6f1b67be1d2d3369c1846979454c860a6ae
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-04-17 19:49:54 +0300

    EXPANDABLE-CHECKBOX-LIST: done

--- src/common.blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/common.blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,5 +1,54 @@
-$('.expandable-checkbox-list__title').click(function(){
-  $(this).parent().find('.expandable-checkbox-list__list').slideToggle();
-  $(this).parent().find('.expandable-checkbox-list__expand-more').toggleClass('expandable-checkbox-list__expand-more_upsidedown');
-  return false;
+class ExpandableCheckboxList {
+
+  constructor(checkboxList){
+    this.ICON_EXPANDED_CLASS = 'expandable-checkbox-list__expand-more_expanded';
+
+    this.checkboxList = checkboxList;
+    this.ExpandMoreButton = this.checkboxList.find('.expandable-checkbox-list__title');
+    this.expandMoreIcon = this.checkboxList.find('.expandable-checkbox-list__expand-more');
+    this.popup = this.checkboxList.find('.expandable-checkbox-list__list');
+    this.expanded = this.expandMoreIcon.hasClass(this.ICON_EXPANDED_CLASS);
+
+    this.setInitialState();
+    this.bindEventListeners();
+  }
+
+  setInitialState() {
+    if(this.expanded){
+      this.expand();
+    }
+    else {
+      this.shrink();
+    }
+  }
+
+  bindEventListeners() {
+    this.ExpandMoreButton.click(this.toggleList.bind(this));
+  }
+
+  toggleList() {
+    if (this.expanded)
+      this.shrink();
+    else
+      this.expand();
+  }
+
+  expand() {
+    this.popup.slideDown();
+    this.expandMoreIcon.addClass(this.ICON_EXPANDED_CLASS);
+    this.expanded = true;
+  }
+
+  shrink() {
+    this.popup.slideUp();
+    this.expandMoreIcon.removeClass(this.ICON_EXPANDED_CLASS);
+    this.expanded = false;
+  }
+
+}
+
+let ExpandableCheckboxLists = []; 
+
+$('.expandable-checkbox-list').each( function(index, element) {
+  ExpandableCheckboxLists[index] = new ExpandableCheckboxList($(element));
 });
