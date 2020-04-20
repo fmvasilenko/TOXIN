50496b2f762135a1407c6defde7651ed33b75f3b
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-04-20 21:25:50 +0300

    DROP-DOWN: created DropDown class

--- src/common.blocks/drop-down/drop-down.js
+++ src/common.blocks/drop-down/drop-down.js
@@ -1,81 +1,206 @@
-/*class DropDown {
+class DropDown {
 
-  bindEventListeners(dropDown) {
-    let expandeMore = dropDown.find('.drop-down__expand-more');
+  constructor(dropDown) {
+    this.dropDown = dropDown;
+    
+    this.setEnvironment();
+    this.setOptionsEnvironment();
 
-    expandeMore.addEventListener('click', this.expandMore);
+    this.bindEventListeners();
   }
 
-  expandMore(event) {
-    let dropDown = $(this).parents('.drop-down');
-    dropDown.find('.drop-down__list').toggleClass('drop-down__list_drop');
-    dropDown.find('.drop-down__field').toggleClass('drop-down__field_drop');
+  setEnvironment() {
+    this.POPUP_EXPANDED_CLASS = 'drop-down__list_drop';
+    this.FIELD_EXPANDED_CLASS = 'drop-down__field_drop';
+
+    this.expandMoreButton = this.dropDown.find('.drop-down__expand-more');
+    this.popup = this.dropDown.find('.drop-down__list');
+    this.field = this.dropDown.find('.drop-down__field');
+    this.fieldInput = this.dropDown.find('.drop-down__field-input');
+    this.clearButton = this.dropDown.find('.js-drop-down__clear-button');
+    this.fieldDefaultValue = this.fieldInput.val();
+    this.expanded = this.popup.hasClass(this.POPUP_EXPANDED_CLASS);
   }
 
-}*/
+  setOptionsEnvironment() {
+    let dropDownOptions = [];
 
-function changeDropdownField($drop_down){ //adding text to dropdown field
-  // $drop_down has the whole dropdown element in it 
-  let str = '';
+    this.dropDown.find('.drop-down__option').each( function(index, element) {
+      dropDownOptions[index] = {};
+      dropDownOptions[index].input = $(element).find('.drop-down__input');
+      dropDownOptions[index].value = dropDownOptions[index].input.val();
+      dropDownOptions[index].increaseButton = $(element).find('.js-drop-down__button_increase');
+      dropDownOptions[index].decreaseButton = $(element).find('.js-drop-down__button_decrease');
+    });
 
-  $drop_down.find('.drop-down__input').each( function (index) { //creating a string
-    if($(this).val() != 0) {
+    this.dropDownOptions = dropDownOptions;
+  }
 
-      if(str != '') str += ', ';
+  bindEventListeners() {
+    this.expandMoreButton.click(this.toggleList.bind(this));
 
-      str += $(this).val();
-      str += ' ' + $(this).parents('.drop-down__option').find('.drop-down__option-title').text();
+    this.dropDown.find('.drop-down__option').each( function(index, element) {
+
+      this.dropDownOptions[index].increaseButton.click( function() {
+        this.increase(index);
+        this.display();
+      }.bind(this));
+
+      this.dropDownOptions[index].decreaseButton.click( function() {
+        this.decrease(index);
+        this.display();
+      }.bind(this));
+
+    }.bind(this));
+
+    this.clearButton.click(this.handlerClearButton.bind(this));
+  }
+
+  toggleList() {
+    if (this.expanded)
+      this.shrink();
+    else
+      this.expand();
+  }
+
+  expand() {
+    this.popup.addClass(this.POPUP_EXPANDED_CLASS);
+    this.field.addClass(this.FIELD_EXPANDED_CLASS);
+    this.expanded = true;
+  }
+
+  shrink() {
+    this.popup.removeClass(this.POPUP_EXPANDED_CLASS);
+    this.field.removeClass(this.FIELD_EXPANDED_CLASS);
+    this.expanded = false;
+  }
+
+  increase(index) {
+    if( this.dropDownOptions[index].value == 0 ) {
+      this.dropDownOptions[index].decreaseButton.prop("disabled", false);
     }
-  })
 
-  if(str != '') $drop_down.find('.drop-down__field-input').val(str); //if str is not empty -> add it to the field
-  else $drop_down.find('.drop-down__field-input').val('Выберите');
-
-  return false;
-};
-
-$('.drop-down__button_increase').click(function(){ //increasing button
-  var $input = $(this).parent().find('input');
-
-  $input.val(parseInt($input.val()) + 1); //increasing the value
-  $input.change();
-
-  $(this).parent().find('.drop-down__button_decrease').prop("disabled", false); //activating the decrease button
-
-  let dropDown = $(this).parents('.drop-down');
-  changeDropdownField(dropDown);
-
-  return false;
-});
-
-$('.drop-down__button_decrease').click(function () {//decreasing button
-  var $input = $(this).parent().find('input');
-  var count = parseInt($input.val()) - 1;
-
-  count = count < 1 ? 0 : count; //decreasing the value
-  $input.val(count);
-  $input.change();
-
-  if(count == 0) {
-    $(this).prop("disabled", true); //disable the decrease button if the value is 0
+    this.dropDownOptions[index].value++;
+    this.dropDownOptions[index].input.val(this.dropDownOptions[index].value);
   }
 
-  let dropDown = $(this).parents('.drop-down');
-  changeDropdownField(dropDown);
-
-  return false;
-});
-
-$('.drop-down__expand-more').click(function(){  //expand-more button
-  let dropDown = $(this).parents('.drop-down');
-  dropDown.find('.drop-down__list').toggleClass('drop-down__list_drop');
-  dropDown.find('.drop-down__field').toggleClass('drop-down__field_drop');
-})
-
-$(document).ready(function(){
-  $('.drop-down__input').each(function(index){  //disabling the decrease buttons after being generated
-    if($(this).val() == 0) {
-      $(this).parent().find('.drop-down__button_decrease').prop("disabled", true);
+  decrease(index) {
+    if( this.dropDownOptions[index].value > 0 ) {
+      this.dropDownOptions[index].value--;
+      this.dropDownOptions[index].input.val(this.dropDownOptions[index].value);
     }
-  })
-})
+
+    if( this.dropDownOptions[index].value == 0 ) {
+      this.dropDownOptions[index].decreaseButton.prop("disabled", true);
+    }
+  }
+
+  num2str(n, forms) {
+    n %= 100;
+
+    if(n < 10 || n > 20) {
+      n %= 10;
+
+      if(n == 1) return forms[0];
+      else if (n > 1 && n < 5) return forms[1];
+      else return forms[2];
+    }
+    else return forms[2];
+  } 
+
+  handlerClearButton() {
+    this.dropDownOptions.forEach(function(item) {
+      item.value = 0;
+      item.input.val(0);
+    }.bind(this));
+
+    this.display();
+  }
+
+  display() {}
+
+}
+
+class DropDownDisplayTotal extends DropDown {
+
+  constructor(dropDown) {
+    super(dropDown);
+    
+    this.setFieldForms();
+
+    this.display();
+  }
+
+  setFieldForms() {
+    let forms = this.fieldInput.attr('forms');
+
+    if(forms) this.fieldForms = forms.split(',');
+    else {
+      forms = [];
+      forms[0] = forms[1] = forms[2] =this.fieldInput.val();
+      this.fieldForms = forms;
+    }
+  }
+
+  display() {
+    let number = 0;
+
+    this.dropDownOptions.forEach(function(item) {
+      number += parseInt(item.value);
+    }.bind(this));
+
+    if(number){
+      let str = number + ' ' + this.num2str(number, this.fieldForms);
+      this.fieldInput.val(str);
+    }
+    else {
+      this.fieldInput.val(this.fieldDefaultValue);
+    }
+  }
+
+}
+
+class DropDownDisplayValues extends DropDown {
+
+  constructor(dropDown) {
+    super(dropDown);
+    
+    this.setFieldForms();
+
+    this.display();
+  }
+
+  setFieldForms() {
+    this.dropDownOptions.forEach(function(item) {
+      let forms = item.input.attr('forms');
+
+      if(forms) item.forms = forms.split(',');
+      else {
+        forms = [];
+        forms[0] = forms[1] = forms[2] = item.input.val();
+        item.forms = forms;
+      }
+    });
+  }
+
+  display() {
+    let str = '';
+
+    this.dropDownOptions.forEach(function(item) {
+      if(item.value != '0') {
+        if(str) str += ', ';
+        str += item.value + ' ' + this.num2str(item.value, item.forms);
+      }
+    }.bind(this));
+
+    if(str) this.fieldInput.val(str);
+    else this.fieldInput.val(this.fieldDefaultValue);
+  }
+
+}
+
+let dropDowns = []; 
+
+$('.drop-down').each( function(index, element) {
+  dropDowns[index] = new DropDownDisplayValues($(element));
+});
