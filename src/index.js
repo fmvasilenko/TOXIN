d09180c90957ca65ec78972b9e95c36f9872d8ac
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-08-10 13:02:34 +0300

    added autoimport for styles #5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './scss/main.scss';
+
+const blocksStyles = require.context('./common.blocks/', true, /\.scss/);
+blocksStyles.keys().forEach(blocksStyles);
+
+const pagesStyles = require.context('./pages/', true, /\.scss/);
+pagesStyles.keys().forEach(pagesStyles);
