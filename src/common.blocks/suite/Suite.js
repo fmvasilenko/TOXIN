7be6c6eda0fb9fd279946eb0956e7c58666b35d9
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-10-13 18:55:41 +0300

    Suite: added publicAPI

--- src/common.blocks/suite/Suite.js
+++ src/common.blocks/suite/Suite.js
@@ -1,18 +1,8 @@
-import RateButton from '../rate-button/RateButton';
-import SuiteLogic from './SuiteLogic';
+import SuiteConnector from './SuiteConnector';
 
 class Suite {
   constructor(container) {
-    this.classes = require('./suite.classes.json');
-    this.DOM = this.findDOMNodes(container);
-    this.suiteLogic = new SuiteLogic(container);
-    this.rate = new RateButton(this.DOM.rate);
-  }
-
-  findDOMNodes(container) {
-    return {
-      rate: container.querySelector(`.${this.classes.rate}`),
-    };
+    new SuiteConnector(container);
   }
 }
 
---
+++ src/common.blocks/suite/SuiteConnector.js
@@ -0,0 +1,19 @@
+import RateButton from '../rate-button/RateButton';
+import SuiteLogic from './SuiteLogic';
+
+class SuiteConnector {
+  constructor(container) {
+    this.classes = require('./suite.classes.json');
+    this.DOM = this.findDOMNodes(container);
+    this.suiteLogic = new SuiteLogic(container);
+    this.rate = new RateButton(this.DOM.rate);
+  }
+
+  findDOMNodes(container) {
+    return {
+      rate: container.querySelector(`.${this.classes.rate}`),
+    };
+  }
+}
+
+export default SuiteConnector;
