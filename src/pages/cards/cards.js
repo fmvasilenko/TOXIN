afc6fa60ec2d5db0c4068d21293626210816d78f
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-10-13 18:49:30 +0300

    changed SearchForm

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,5 +1,6 @@
 import Calendar from '../../common.blocks/calendar/Calendar';
 import Receipt from '../../common.blocks/receipt/Receipt';
+import SearchForm from '../../common.blocks/search-form/SearchForm';
 import Suite from '../../common.blocks/suite/Suite';
 import '../../scss/main.scss';
 // import CardsController from './CardsController';
@@ -21,3 +22,16 @@ console.log(new Suite(suite));
 const receiptContainer = document.querySelector('.cards__receipt');
 const receipt = new Receipt(receiptContainer);
 receipt.setTotalPriceSubscriber((value) => console.log(value));
+
+const searchFormContainer = document.querySelector('.cards__search-form');
+const searchForm = new SearchForm(searchFormContainer);
+/*
+searchForm.setArrivalDate(new Date('2020-10-20'));
+searchForm.setLeavingDate(new Date('2020-10-30'));
+console.log(searchForm.getArrivalDate());
+console.log(searchForm.getLeavingDate());
+searchForm.setArrivalDateSubscriber((date) => console.log(date));
+searchForm.setLeavingDateSubscriber((date) => console.log(date));
+console.log(searchForm.getTotalGuestsNumber());
+searchForm.setTotalGuestsNumberSubscriber((value) => console.log(value));
+*/
