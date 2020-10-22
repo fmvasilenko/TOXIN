a36d13f15f3b5b15ae1ad57a4f85e9368a1e3ca6
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2020-10-22 15:13:31 +0300

    Cards: added eventListeners for calendar

--- src/pages/cards/CardsConnector.js
+++ src/pages/cards/CardsConnector.js
@@ -2,16 +2,24 @@ import SearchForm from '@blocks/search-form/SearchForm';
 import Receipt from '@blocks/receipt/Receipt';
 import Calendar from '@blocks/calendar/Calendar';
 import Suite from '@blocks/suite/Suite';
+import CardsLogic from './CardsLogic';
 
 class CardsConnector {
   constructor(container) {
     this.classes = require('./cards.classes.json');
     this.DOM = this.findDOMNodes(container);
 
+    this.cardsLogic = new CardsLogic(container);
     this.searchForm = new SearchForm(this.DOM.searchForm);
     this.recipt = new Receipt(this.DOM.receipt);
     this.calendar = new Calendar(this.DOM.calendar);
     this.suites = this.getSuites();
+
+    this.defineSubscriptions();
+  }
+
+  defineSubscriptions() {
+    this.cardsLogic.setCalendarClickSubscriber(this.calendar.clickHandler);
   }
 
   findDOMNodes(container) {
---
+++ src/pages/cards/CardsLogic.js
@@ -0,0 +1,28 @@
+class CardsLogic {
+  constructor(container) {
+    this.classes = require('./cards.classes.json');
+    this.DOM = this.findDOMNodes(container);
+    this.calendarClickSubscriber = () => {};
+    this.bindEventListeners();
+  }
+
+  setCalendarClickSubscriber(subscriber) {
+    this.calendarClickSubscriber = subscriber;
+  }
+
+  findDOMNodes(container) {
+    return {
+      calendar: container.querySelector(`.${this.classes.calendar}`),
+    };
+  }
+
+  bindEventListeners() {
+    this.DOM.calendar.addEventListener('click', this.calendarClickHandler.bind(this));
+  }
+
+  calendarClickHandler(event) {
+    this.calendarClickSubscriber(event);
+  }
+}
+
+export default CardsLogic;
